3c28adcece076de635810502d95efacf58f67c79
PacketLoss10 <dinsleyowen@gmail.com>
2026-01-23 17:36:25 +0000

    Initial Project Commit

---
+++ JourneymanProject1/Source/JourneymanProject1/JourneymanProject1.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JourneymanProject1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JourneymanProject1, "JourneymanProject1" );
+ 
