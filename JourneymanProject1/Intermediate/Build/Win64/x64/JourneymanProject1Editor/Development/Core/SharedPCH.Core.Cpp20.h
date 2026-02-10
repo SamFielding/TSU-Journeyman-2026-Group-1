9a5907fc134bf5579cef33b4d6d137046365f8e3
SamFielding <148544615+SamFielding@users.noreply.github.com>
2026-02-10 10:59:55 +0000

    Initial commit

---
+++ JourneymanProject1/Intermediate/Build/Win64/x64/JourneymanProject1Editor/Development/Core/SharedPCH.Core.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "C:/Users/e4086040/Documents/GitHub/TSU-Journeyman-2026-Group-1/JourneymanProject1/Intermediate/Build/Win64/x64/JourneymanProject1Editor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
--- README.md
+++
@@ -1,2 +0,0 @@
-# TSU-Journeyman-2026-Group-1
-Public github repo for group 1s project work, pull only (clone if you want to mess with it)
