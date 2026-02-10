b0323fc95858f405ced91f581ad7f191342f2374
SamFielding <148544615+SamFielding@users.noreply.github.com>
2026-02-10 11:09:59 +0000

    inital commit

---
+++ JourneymanProject1/Intermediate/Build/Win64/UnrealEditor/Inc/JourneymanProject1/UHT/JourneymanProject1Character.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "JourneymanProject1Character.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef JOURNEYMANPROJECT1_JourneymanProject1Character_generated_h
+#error "JourneymanProject1Character.generated.h already included, missing '#pragma once' in JourneymanProject1Character.h"
+#endif
+#define JOURNEYMANPROJECT1_JourneymanProject1Character_generated_h
+
+#define FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1Character_h_21_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAJourneymanProject1Character(); \
+	friend struct Z_Construct_UClass_AJourneymanProject1Character_Statics; \
+public: \
+	DECLARE_CLASS(AJourneymanProject1Character, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/JourneymanProject1"), NO_API) \
+	DECLARE_SERIALIZER(AJourneymanProject1Character)
+
+
+#define FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1Character_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	AJourneymanProject1Character(AJourneymanProject1Character&&); \
+	AJourneymanProject1Character(const AJourneymanProject1Character&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AJourneymanProject1Character); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AJourneymanProject1Character); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AJourneymanProject1Character) \
+	NO_API virtual ~AJourneymanProject1Character();
+
+
+#define FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1Character_h_18_PROLOG
+#define FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1Character_h_21_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1Character_h_21_INCLASS_NO_PURE_DECLS \
+	FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1Character_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> JOURNEYMANPROJECT1_API UClass* StaticClass<class AJourneymanProject1Character>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1Character_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ JourneymanProject1/Intermediate/Build/Win64/UnrealEditor/Inc/JourneymanProject1/UHT/JourneymanProject1GameMode.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "JourneymanProject1GameMode.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef JOURNEYMANPROJECT1_JourneymanProject1GameMode_generated_h
+#error "JourneymanProject1GameMode.generated.h already included, missing '#pragma once' in JourneymanProject1GameMode.h"
+#endif
+#define JOURNEYMANPROJECT1_JourneymanProject1GameMode_generated_h
+
+#define FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1GameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAJourneymanProject1GameMode(); \
+	friend struct Z_Construct_UClass_AJourneymanProject1GameMode_Statics; \
+public: \
+	DECLARE_CLASS(AJourneymanProject1GameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/JourneymanProject1"), JOURNEYMANPROJECT1_API) \
+	DECLARE_SERIALIZER(AJourneymanProject1GameMode)
+
+
+#define FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1GameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	AJourneymanProject1GameMode(AJourneymanProject1GameMode&&); \
+	AJourneymanProject1GameMode(const AJourneymanProject1GameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(JOURNEYMANPROJECT1_API, AJourneymanProject1GameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AJourneymanProject1GameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AJourneymanProject1GameMode) \
+	JOURNEYMANPROJECT1_API virtual ~AJourneymanProject1GameMode();
+
+
+#define FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1GameMode_h_9_PROLOG
+#define FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1GameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1GameMode_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1GameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> JOURNEYMANPROJECT1_API UClass* StaticClass<class AJourneymanProject1GameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_e4086040_Documents_GitHub_TSU_Journeyman_2026_Group_1_JourneymanProject1_Source_JourneymanProject1_JourneymanProject1GameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
